d5e56a820bff23be88460aaacad15aa9696d5867
usopp9 <usopp9@naver.com>
2020-02-03 20:01:52 +0900

    delete node

--- node_modules/shoes/lib/shoes.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    Server: require('./server').Server
-  , Environment: require('./environment').Environment
-  , express: require('express')
-};
-
--- node_modules/shoes/test/data/modules/foo.js
+++
@@ -1,3 +0,0 @@
-module.exports = function (str) {
-    return 'foo' + str;
-};
